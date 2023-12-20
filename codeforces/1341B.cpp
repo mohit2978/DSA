7461ffcdac7dcf23f3d64684f70c6cc6612afbb6
mohit <mohitrakheja007@gmail.com>
2023-12-20 22:40:26 +0530

    initial commit

---
+++ codeforces/1341B.cpp
@@ -0,0 +1,76 @@
+#include<bits/stdc++.h>
+using namespace std;
+//https://codeforces.com/problemset/problem/1341/B
+#define ff              first
+#define ss              second
+#define lo             long long
+#define ll 				long long int
+#define pb              push_back
+#define mp              make_pair
+#define pii             pair<int,int>
+#define vi              vector<int>
+#define mii             map<int,int>
+#define umii			unordered_map<int, int>
+#define pq_max          priority_queue<int>
+#define pq_min          priority_queue<int,vi,greater<int> >
+#define setbits(x)      __builtin_popcountll(x)
+#define zrobits(x)      __builtin_ctzll(x)
+#define mod             1000000007
+#define inf             1e18
+#define ps(x,y)         fixed<<setprecision(y)<<x
+#define mid(s,e)         (s+(e-s)/2)
+#define mk(arr,n,type)  type *arr=new type[n];
+#define w(t)            int t; cin>>t; while(t--)
+#define DEBUG(x) 		cout << '>' << #x << ':' << x << endl;
+#define REP(i,n) 		for(int i=0;i<(n);i++)
+#define FOR(i,a,b) 		for(int i=(a);i<=(b);i++)
+#define FORD(i,a,b) 	for(int i=(a);i>=(b);i--)
+void solve(int *arr,int k,int size){
+    int res=0,resIndex=1;
+    int peaks=0;
+    for(int i=2;i<=k-1;i++){
+        if(arr[i]>arr[i-1] && arr[i]>arr[i+1]){
+            peaks++;
+        }
+    }
+    res=peaks;
+    int l=1,r=k;
+    while(r<size){
+        if(r!=size-1 &&arr[r]>arr[r-1]&& arr[r]>arr[r+1]) peaks++;
+        r++;
+        l++;
+        if(arr[l]>arr[l-1]&& arr[l]>arr[l+1]) peaks--;
+        if(peaks>res){
+            res=peaks;
+            resIndex=l;
+        }
+    }
+    cout<< res+1 <<" "<<resIndex<<endl;
+}
+
+void fio()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+}
+
+int main(int argc, char const *argv[]) {
+    fio();
+    int tc;
+    cin>>tc;
+    for(int t=0;t<tc;t++){
+        int n,k;
+        cin>>n>>k;
+        int arr[n+1];
+        arr[0]=0;
+        for(int i=1;i<=n;i++){
+            cin>>arr[i];
+        }
+        solve(arr,k,n+1);
+    }
+    return 0;
+}
